d34f8bca7d454d24ea7574368083ebdfd349540e
Camal Cakar <camal_cakar@yahoo.de>
2017-08-08 21:16:25 +0200

    Correct require statement

--- examples/pir/pir.js
+++ examples/pir/pir.js
@@ -1,10 +1,10 @@
-var Gpio = require('onoff.Gpio'),
+var Gpio = require('onoff').Gpio,
     sensor = new Gpio(17, 'in', 'both');
 
 sensor.watch(function(err, value) {
     if (err)
         exit(err);
-    console.log(value ? 'Someone entered your kingdom!' : 'no one here.')
+    console.log(value ? 'Someone entered your kingdom!' : 'no one here.');
 });
 
 function exit(err) {
