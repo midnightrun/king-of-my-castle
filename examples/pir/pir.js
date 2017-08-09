963b3cf7395d39aa6113dbb6201c88f04bd67936
Camal Cakar <camal_cakar@yahoo.de>
2017-08-09 23:27:47 +0200

    Combine PIR with LED example

--- examples/pir/pir.js
+++ examples/pir/pir.js
@@ -1,10 +1,24 @@
 var Gpio = require('onoff').Gpio,
-    sensor = new Gpio(17, 'in', 'both');
+    sensor = new Gpio(17, 'in', 'both'),
+    led = new Gpio(4, 'out');
 
 sensor.watch(function(err, value) {
     if (err)
         exit(err);
-    console.log(value ? 'Someone entered your kingdom!' : 'no one here.');
+
+    if (value  === true) {
+        console.log('Someone entered your kingdom!')
+        led.write(value, function() {
+            console.log('LED state: ' + 1);
+        });
+    } 
+    else {
+        console.log('No one here.')
+        led.write(value, function() {
+            console.log('LED state: ' + 0);
+        });
+    }
+
 });
 
 function exit(err) {
